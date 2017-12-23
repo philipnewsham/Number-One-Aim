7a0892527784fbf15fca780e9e752ec23a0d6803
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-12-23 02:20:07 +0000

    addded stuff

--- Assets/GrowlerPlaythroughUpdated.cs
+++ Assets/GrowlerPlaythroughUpdated.cs
@@ -6,10 +6,20 @@ public class GrowlerPlaythroughUpdated : MonoBehaviour
 {
     public GoToNode[] panelScripts;
     public float[] waitTimes;
+    public bool testing;
+
     public void BeginSong()
     {
-        Invoke("StartSong", 1.0f);
-        StartCoroutine(PanelAnimation());
+        if (!testing)
+        {
+            Invoke("StartSong", 1.0f);
+            StartCoroutine(PanelAnimation());
+        }
+        else
+        {
+            StartSong();
+            StartCoroutine(SinglePanelsTwo());
+        }
     }
 
     void StartSong()
@@ -19,6 +29,8 @@ public class GrowlerPlaythroughUpdated : MonoBehaviour
 
     IEnumerator PanelAnimation()
     {
+        //Time.timeScale = 2.0f;
+        //GetComponent<AudioSource>().pitch = 2.0f;
         yield return new WaitForSeconds(waitTimes[0]);
         StartCoroutine(PanelSlideIn());
         yield return new WaitForSeconds(GetWaitTime(1));
@@ -29,6 +41,25 @@ public class GrowlerPlaythroughUpdated : MonoBehaviour
         StartCoroutine(SinglePanels());
         yield return new WaitForSeconds(GetWaitTime(4));
         StartCoroutine(DoublePanels());
+        yield return new WaitForSeconds(GetWaitTime(5));
+        StartCoroutine(ExpandOut());
+        yield return new WaitForSeconds(GetWaitTime(6));
+        StartCoroutine(Triangle());
+        //Time.timeScale = 1.0f;
+        //GetComponent<AudioSource>().pitch = 1.0f;
+        yield return new WaitForSeconds(GetWaitTime(7));
+        StartCoroutine(ExpandOut());
+        yield return new WaitForSeconds(GetWaitTime(8));
+        Debug.Log(GetComponent<AudioSource>().time);
+        StartCoroutine(SinglePanelsTwo());
+        yield return new WaitForSeconds(GetWaitTime(9));
+        StartCoroutine(MexicanWave());
+        yield return new WaitForSeconds(GetWaitTime(10));
+        StartCoroutine(SwappingTriangles());
+        yield return new WaitForSeconds(GetWaitTime(11));
+        StartCoroutine(RandomPanels());
+        yield return new WaitForSeconds(GetWaitTime(12));
+        StartCoroutine(ExpandOut());
     }
 
     IEnumerator PanelSlideIn()
@@ -43,15 +74,17 @@ public class GrowlerPlaythroughUpdated : MonoBehaviour
 
     IEnumerator ShrinkIn()
     {
+        yield return new WaitForSeconds(1.0f);
         for (int i = 0; i < 8; i++)
         {
-            panelScripts[i].StartMove(0, 4.0f);
+            panelScripts[i].StartMove(0, 3.0f);
             yield return null;
         }
     }
 
     IEnumerator ExpandOut()
     {
+        yield return new WaitForSeconds(1f);
         for (int i = 0; i < 8; i++)
         {
             panelScripts[i].StartMove(2, 1.0f);
@@ -61,46 +94,160 @@ public class GrowlerPlaythroughUpdated : MonoBehaviour
 
     IEnumerator SinglePanels()
     {
-        int[] panelNo = new int[3] { 1, 3, 6 };
-        for (int i = 0; i < 3; i++)
+        yield return new WaitForSeconds(2.0f);
+        int[] panelNo = new int[4] { 1, 3, 5,7 };
+        int i = panelNo[0];
+        panelScripts[i].StartMove(0, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+        panelScripts[i].StartMove(2, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(2.0f);
+
+        i = panelNo[1];
+        panelScripts[i].StartMove(1, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+        panelScripts[i].StartMove(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.5f);
+        panelScripts[i].StartMove(2, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+
+        i = panelNo[2];
+        panelScripts[i].StartMove(0, 1.5f);
+        yield return new WaitForSeconds(1.5f);
+        panelScripts[i].StartMove(2, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(2.0f);
+
+        i = panelNo[3];
+        panelScripts[i].StartMove(1, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+        panelScripts[i].StartMove(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.5f);
+        panelScripts[i].StartMove(2, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+    }
+    public float tempWait;
+    IEnumerator DoublePanels()
+    {
+        yield return new WaitForSeconds(1f);
+        panelScripts[1].StartMove(1, 2.5f);
+        panelScripts[5].StartMove(1, 2.5f);
+        yield return new WaitForSeconds(2.5f);
+        panelScripts[1].StartMove(0, .5f);
+        panelScripts[5].StartMove(0, .5f);
+        yield return new WaitForSeconds(2.0f);
+        panelScripts[1].StartMove(2, 2.0f);
+        panelScripts[5].StartMove(2, 2.0f);
+
+        yield return new WaitForSeconds(2.0f);
+        panelScripts[3].StartMove(1, 2.5f);
+        panelScripts[7].StartMove(1, 2.5f);
+        yield return new WaitForSeconds(2.5f);
+        panelScripts[3].StartMove(0, .5f);
+        panelScripts[7].StartMove(0, .5f);
+        yield return new WaitForSeconds(2.0f);
+        panelScripts[3].StartMove(2, 2.0f);
+        panelScripts[7].StartMove(2, 2.0f);
+    }
+
+    IEnumerator Triangle()
+    {
+        panelScripts[1].StartMove(1, 1.0f);
+        panelScripts[3].StartMove(1, 1.0f);
+        panelScripts[6].StartMove(1, 1.0f);
+        yield return new WaitForSeconds(5.0f);
+        panelScripts[1].StartMove(2, .2f);
+        panelScripts[3].StartMove(2, .2f);
+        panelScripts[6].StartMove(2, .2f);
+    }
+
+    IEnumerator SinglePanelsTwo()
+    {
+        yield return new WaitForSeconds(1.0f);
+        int[] panelNo = new int[2] { 0, 4};
+        int i = panelNo[0];
+        yield return new WaitForSeconds(0.5f);
+        panelScripts[i].StartMove(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        panelScripts[i].StartMove(2, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(5.0f);
+
+        i = panelNo[1];
+        yield return new WaitForSeconds(0.5f);
+        panelScripts[i].StartMove(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        panelScripts[i].StartMove(2, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    IEnumerator MexicanWave()
+    {
+        yield return new WaitForSeconds(2.0f);
+        for (int k = 0; k < 2; k++)
         {
-            panelScripts[panelNo[i]].StartMove(0, 3.0f);
-            yield return new WaitForSeconds(3.2f);
-            panelScripts[panelNo[i]].StartMove(2, 1f);
-            yield return new WaitForSeconds(1f);
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    panelScripts[i].StartMove(j, (2.0f - 0.46f) / 8.0f);
+                    yield return new WaitForSeconds(0.2f);
+                }
+                for (int i = 0; i < 8; i++)
+                {
+                    panelScripts[i].StartMove(2, (2.0f - 0.46f) / 8.0f);
+                    yield return new WaitForSeconds(0.2f);
+                }
+            }
         }
     }
 
-    IEnumerator DoublePanels()
+    IEnumerator SwappingTriangles()
     {
-        yield return new WaitForSeconds(2.2f);
-        panelScripts[1].StartMove(0, 1.0f);
-        panelScripts[5].StartMove(0, 1.0f);
+        for (int i = 0; i < 2; i++)
+        {
+            panelScripts[1].StartMove(1, 1.0f);
+            panelScripts[3].StartMove(1, 1.0f);
+            panelScripts[6].StartMove(1, 1.0f);
+            yield return new WaitForSeconds(2.0f);
+            panelScripts[1].StartMove(2, 1.0f);
+            panelScripts[3].StartMove(2, 1.0f);
+            panelScripts[6].StartMove(2, 1.0f);
+            yield return new WaitForSeconds(2.0f);
+
+            panelScripts[2].StartMove(1, 1.0f);
+            panelScripts[5].StartMove(1, 1.0f);
+            panelScripts[7].StartMove(1, 1.0f);
+            yield return new WaitForSeconds(2.0f);
+            panelScripts[2].StartMove(2, 1.0f);
+            panelScripts[5].StartMove(2, 1.0f);
+            panelScripts[7].StartMove(2, 1.0f);
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+
+    IEnumerator RandomPanels()
+    {
         yield return new WaitForSeconds(2.0f);
-        panelScripts[1].StartMove(2, 1.0f);
-        panelScripts[5].StartMove(2, 1.0f);
-
-        yield return new WaitForSeconds(3.2f);
-        panelScripts[3].StartMove(0, 1.0f);
-        panelScripts[7].StartMove(0, 1.0f);
-        yield return new WaitForSeconds(2.0f);
-        panelScripts[3].StartMove(2, 1.0f);
-        panelScripts[7].StartMove(2, 1.0f);
-
-
-        /*
-        int[] panelNo = new int[4] { 1, 3, 0, 2 };
         for (int i = 0; i < 4; i++)
         {
-            panelScripts[panelNo[i]].StartMove(0, 1.0f);
-            panelScripts[panelNo[i]+4].StartMove(0, 1.0f);
-            yield return new WaitForSeconds(3.5f);
-            panelScripts[panelNo[i]].StartMove(2, 1.0f);
-            panelScripts[panelNo[i]+4].StartMove(2, 1.0f);
-            yield return new WaitForSeconds(1.5f);
-        }
-        */
+            int a = Random.Range(0, 8);
+            int b = Random.Range(0, 8);
+            while(b==a)
+            {
+                b = Random.Range(0, 8);
+                yield return null;
+            }
 
+            panelScripts[a].StartMove(Random.Range(0,2), 0.5f);
+            panelScripts[b].StartMove(Random.Range(0,2), 0.5f);
+            yield return new WaitForSeconds(2.0f);
+            panelScripts[a].StartMove(2, 0.5f);
+            panelScripts[b].StartMove(2, 0.5f);
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 
     float GetWaitTime(int currentNumber)
---
+++ Assets/ParentMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParentMove : MonoBehaviour
+{
+    public GameObject[] nodes;
+    private int currentNode;
+
+    void Start()
+    {
+        currentNode = 0;
+    }
+
+    public void StartMove(int nodeNo, float moveTime)
+    {
+        StartCoroutine(MoveToNode(nodeNo, moveTime));
+    }
+
+    IEnumerator MoveToNode(int nodeNo, float moveTime)
+    { 
+        float lerpTime = 0.0f;
+        float lerpAmount = (1.0f / 7.0f)*Time.deltaTime;
+        while (lerpTime <= 1.0f)
+        {
+            Vector3 startPosition = nodes[currentNode].transform.position;
+            Vector3 endPosition = nodes[nodeNo].transform.position;
+
+            float posX = Mathf.Lerp(startPosition.x, endPosition.x, lerpTime);
+            float posY = Mathf.Lerp(startPosition.y, endPosition.y, lerpTime);
+            float posZ = Mathf.Lerp(startPosition.z, endPosition.z, lerpTime);
+
+            transform.position = new Vector3(posX, posY, posZ);
+
+            lerpTime += Time.deltaTime / moveTime;
+            yield return new WaitForEndOfFrame();
+        }
+        currentNode = nodeNo;
+        transform.position = nodes[nodeNo].transform.position;
+    }
+}
